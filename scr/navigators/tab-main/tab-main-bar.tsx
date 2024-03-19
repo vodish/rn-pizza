0910d0b5bd837a48d1ca9813da681a2281234bff
pkarasev <p.karasev@psw.ru>
2024-03-19 15:54:31 +0300

    upd

--- scr/navigators/tab-main/tab-main-bar.tsx
+++ scr/navigators/tab-main/tab-main-bar.tsx
@@ -1,12 +1,21 @@
 import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
 import { StyleSheet, View, TouchableOpacity, Text } from 'react-native'
+import SvgLocationOn from '../../components/svg/svg_location_on';
+import SvgLunchDining from '../../components/svg/svg_lunch_dining';
+import SvgShoppingBasket from '../../components/svg/svg_shopping_basket';
+import SvgClockLoader from '../../components/svg/svg_clock_loader';
+import SvgFace from '../../components/svg/svg_face';
+import SvgHelp from '../../components/svg/svg_help';
+
 
 // настройка табов
 
 export default function TbaBarMain({ state, descriptors, navigation }: BottomTabBarProps) {
 
-  // console.log(JSON.stringify(state.routes, null, 4))
-  console.log(JSON.stringify(descriptors, null, 4))
+  // console.log(JSON.stringify(state, null, 4))
+  // console.log(JSON.stringify(descriptors, null, 4))
+
+
 
   return (
 
@@ -15,8 +24,8 @@ export default function TbaBarMain({ state, descriptors, navigation }: BottomTab
         state.routes.map((route, index) => {
 
           const { options } = descriptors[route.key];
-
           const isFocused = state.index === index;
+
 
           const onPress = () => {
             const event = navigation.emit({
@@ -47,10 +56,17 @@ export default function TbaBarMain({ state, descriptors, navigation }: BottomTab
               onPress={onPress}
               onLongPress={onLongPress}
               // style={{ flex: 1, backgroundColor: isFocused ? '#ccc' : '#fff' }}
-              style={style.td}
+              style={style.cell}
             >
-              {/* <Text style={{ color: isFocused ? 'red' : '#222', fontSize: 13, textAlign: 'center', paddingVertical: 10 }}>{options.title}</Text> */}
+
+              {route.name == 'StackAddress' && <SvgLocationOn fill={isFocused ? 'red' : ''} />}
+              {route.name == 'DrawCatalog' && <SvgLunchDining fill={isFocused ? 'red' : ''} />}
+              {route.name == 'StackBasket' && <SvgShoppingBasket fill={isFocused ? 'red' : ''} />}
+              {route.name == 'StackAction' && <SvgClockLoader fill={isFocused ? 'red' : ''} />}
+              {route.name == 'StackProfile' && <SvgHelp fill={isFocused ? 'red' : ''} />}
+
               <Text style={style.title}>{options.title}</Text>
+
             </TouchableOpacity>
           );
         })
@@ -61,7 +77,8 @@ export default function TbaBarMain({ state, descriptors, navigation }: BottomTab
 }
 
 const style = StyleSheet.create({
-  bar: { flexDirection: 'row', alignItems: 'flex-end' },
-  td: { flex: 1, borderWidth: 1, borderColor: 'red', },
-  title: { color: '#222', fontSize: 12, textAlign: 'center' },
+  bar: { flexDirection: 'row', justifyContent: 'space-around', paddingTop: 5, paddingBottom: 3, },
+  cell: { flex: 1, textAlign: 'center', borderWidth: 0, borderColor: 'red', display: 'flex', flexDirection: 'column', alignItems: 'center', },
+  title: { color: '#222', fontSize: 12, borderWidth: 0, borderColor: 'red' },
+
 })
--- scr/navigators/tab-main/tab-main.tsx
+++ scr/navigators/tab-main/tab-main.tsx
@@ -6,6 +6,7 @@ import StackAction from '../stack-action/stack-action';
 import StackBasket from '../stack-basket/stack-basket';
 import StackProfile from '../stack-profile/stack-profile';
 import TbaBarMain from './tab-main-bar';
+import SvgLocationOn from '../../components/svg/svg_location_on';
 
 
 const { Navigator, Screen } = createBottomTabNavigator();
@@ -15,6 +16,7 @@ export default function TabMain() {
 
   return (
     <Navigator screenOptions={option} initialRouteName="StackAction" tabBar={props => <TbaBarMain {...props} />}>
+      <Screen name="StackAddress" component={StackBasket} options={{title: 'Адрес'}} />
       <Screen name="DrawCatalog" component={DrawCatalog} options={{title: 'Каталог'}} />
       <Screen name="StackBasket" component={StackBasket} options={{title: 'Корзина'}} />
       <Screen name="StackAction" component={StackAction} options={{title: 'Акции'}} />
