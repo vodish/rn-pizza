fdb504bc04decf26171ba61362aec27b85a421ee
pkarasev <p.karasev@psw.ru>
2024-03-26 15:58:02 +0300

    upd

---
+++ scr/screens/address/filial/card/screen-address-filial-card.style.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+
+export const style = StyleSheet.create({
+  row: {
+    // borderWidth: 1, borderColor: 'red',
+    display: 'flex', flexDirection: 'row', justifyContent: 'space-between',
+  },
+  two: {
+    // borderWidth: 1, borderColor: 'red',
+    display: 'flex', flexDirection: 'row', justifyContent: 'space-between',
+  },
+  
+})
